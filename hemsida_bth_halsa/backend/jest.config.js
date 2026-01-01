581bdfbb14e12a221762151fd2a463ac36e9e721
Abod <112323349+aabb00dd@users.noreply.github.com>
2026-01-01 15:35:03 +0100

    Delete hemsida_bth_halsa directory

--- hemsida_bth_halsa/backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    rootDir: "..", // Move up one level from backend
-    testMatch: ["<rootDir>/unit_tests/**/*.test.js"], // Match all test files inside unit_tests
-    moduleDirectories: ["<rootDir>/backend/node_modules", "node_modules"],
-    testEnvironment: "node",
-  };
-  
--- hemsida_bth_halsa/unit_tests/backend/routes/generalRoutes.test.js
+++
@@ -1,1 +0,0 @@
-test.skip("This test is a placeholder", () => {});
